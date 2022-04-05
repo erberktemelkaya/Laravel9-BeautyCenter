ec50ed61a8f60c551dee66982505708b26e883bd
erberktemelkaya <erberktemelkaya@gmail.com>
2022-04-06 02:31:38 +0300

    page changed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
+use App\Http\Controllers\AdminPanel\CategoryController as AdminCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,4 +31,11 @@ Route::get('/deneme',[TestController::class,'deneme']);
 
 
 Route::get('/admin',[AdminHomeController::class,'index'])->name(name:'admin');
+
+Route::get('/admin/category',[AdminCategoryController::class,'index'])->name(name:'admin_category');
+Route::get('/admin/category/create',[AdminCategoryController::class,'create'])->name(name:'admin_category_create');
+Route::post('/admin/category/store',[AdminCategoryController::class,'store'])->name(name:'admin_category_store');
+Route::get('/admin/category/edit/{id}',[AdminCategoryController::class,'edit'])->name(name:'admin_category_edit');
+Route::post('/admin/category/update/{id}',[AdminCategoryController::class,'update'])->name(name:'admin_category_update');
+Route::get('/admin/category/show/{id}',[AdminCategoryController::class,'show'])->name(name:'admin_category_show');
 
